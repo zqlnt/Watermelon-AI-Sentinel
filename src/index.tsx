8e0ee480f808ab014a2342abfb55c5d393073098
zqlnt <zaingul@hotmail.co.uk>
2025-08-21 16:55:50 +0100

    Redesign Watermelon AI Sentinel with NovaOps Template

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { StellarDesign } from "./screens/StellarDesign/StellarDesign";
-
-createRoot(document.getElementById("app") as HTMLElement).render(
-  <StrictMode>
-    <StellarDesign />
-  </StrictMode>,
-);
